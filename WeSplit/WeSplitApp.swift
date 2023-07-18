b1c10835097360e59aea5bacd1936263ab5a60fe
jezsdaniel <jadendaniel7@gmail.com>
2023-07-18 16:44:51 -0400

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by J. Daniel F. Ruano on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
